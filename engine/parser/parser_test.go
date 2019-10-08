0632d57e6080e2419ee148c6a73ad52bc061f38e
amirrezaask <raskarpour@gmail.com>
2019-10-08 23:17:51 +0330

    fix test

--- engine/parser/parser_test.go
+++ engine/parser/parser_test.go
@@ -12,11 +12,18 @@ func TestParse(t *testing.T) {
 func Test_shouldContinue(t *testing.T) {
 
 	code1 := "fmt.Println(\n"
-	assert.True(t, ShouldContinue(code1))
+	i, sc := ShouldContinue(code1)
+	assert.True(t, sc)
+	assert.Equal(t, 1, i)
 	code2 := "fmt.Println(fmt.Sprint(2"
-	assert.True(t, ShouldContinue(code2))
+	i, sc = ShouldContinue(code2)
+	assert.True(t, sc)
+	assert.Equal(t, 2, i)
 	code3 := "{fmt.Print("
-	assert.True(t, ShouldContinue(code3))
+	i, sc = ShouldContinue(code3)
+	assert.True(t, sc)
+	assert.Equal(t, 2, i)
 	code4 := "fmt.Println(22)"
-	assert.False(t, ShouldContinue(code4))
+	i, sc = ShouldContinue(code4)
+	assert.False(t, sc)
 }
