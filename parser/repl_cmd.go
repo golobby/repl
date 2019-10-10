245bdbeac9899bcc5980e127bb069cc89fc12dc5
amirrezaask <raskarpour@gmail.com>
2019-10-10 15:07:23 +0330

    add go doc and infrastructure for more commands

--- parser/repl_cmd.go
+++ parser/repl_cmd.go
@@ -1,11 +1,23 @@
 package parser
 
+import "strings"
+
 type REPLCmd uint8
 
 const (
 	REPLCmdDoc = REPLCmd(iota)
 )
 
-func ParseCmd() {
+func ParseCmd(code string) (REPLCmd, string) {
+	if isGoDoc(code) {
+		return REPLCmdDoc, strings.Split(code, " ")[1]
+	}
+	return 0, ""
+}
 
+func isGoDoc(code string) bool {
+	if len(code) == 0 {
+		return false
+	}
+	return code[:4] == ":doc"
 }
