4334ebe2632983c537690b037d8d8c9e8b675d15
amirrezaask <raskarpour@gmail.com>
2019-10-13 20:00:58 +0330

    add test for types

---
+++ session/types_test.go
@@ -0,0 +1,18 @@
+package session
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_isTypeDecl(t *testing.T) {
+	assert.True(t, isTypeDecl("type user struct{\n Name string \n}"))
+	assert.True(t, isTypeDecl("type user interface{\n Name() string \n}"))
+	assert.False(t, isTypeDecl("aaaaa"))
+}
+
+func Test_ExtractTypeName(t *testing.T) {
+	assert.Equal(t, "user", ExtractTypeName("type user struct{\n Name string \n}"))
+	assert.Equal(t, "user", ExtractTypeName("type user interface{\n Name() string \n}"))
+}
